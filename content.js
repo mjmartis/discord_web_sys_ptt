2ed5935026c87a7046127a5e294a9b8195b9a10e
Michael Martis <michaeljmartis@gmail.com>
2020-07-28 17:51:15 +0800

    Event dispatch test

--- content.js
+++ content.js
@@ -6,8 +6,19 @@ const script = document.createElement("script");
 // to local storage.
 script.innerHTML = `
 window.localStorage.__proto__ = Object.create(Storage.prototype);
-window.localStorage.__proto__.setItem = function(k, v) {
-  console.log("LS SET CALLED " + k);
+window.localStorage.__proto__.setItem = function(key, value) {
+  if (key === 'SelectedChannelStore') {
+    const broadcasting = JSON.parse(value).selectedVoiceChannelId !== null;
+    document.dispatchEvent(new CustomEvent('SwpttBroadcasting',
+      {broadcasting: broadcasting}));
+    console.log('!! BROADCASTING ' + broadcasting);
+  } else if (key === 'MediaEngineStore') {
+    const store = JSON.parse(value).default;
+    const shortcut = store.mode == 'PUSH_TO_TALK' ? store.modeOptions.shortcut : null;
+    document.dispatchEvent(new CustomEvent('SwpttShortcut',
+      {shortcut: shortcut}));
+    console.log('!! SHORTCUT ' + shortcut);
+  }
   Storage.prototype.setItem.apply(this, arguments);
 }
 `;
