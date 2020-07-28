f4239029212e01921b621b600364ba16876f8b21
Michael Martis <michaeljmartis@gmail.com>
2020-07-28 23:24:51 +0800

    Only dispatch on updated shortcut

--- injected.js
+++ injected.js
@@ -1,17 +1,44 @@
-// Override method to notify us about local storage changes.
-window.localStorage.__proto__ = Object.create(Storage.prototype);
-window.localStorage.__proto__.setItem = function(key, value) {
-  if (key === 'MediaEngineStore') {
-    try {
-      const store = JSON.parse(value).default;
-      const shortcut = store.mode == 'PUSH_TO_TALK' ? store.modeOptions.shortcut : undefined;
-      document.dispatchEvent(new CustomEvent('SwpttShortcutChanged', {
-        'detail': shortcut
-      }));
-    } catch (err) {
-      console.error('Error in local storage update: ' + err);
-    }
+// Compare two (possibly nested) arrays.
+function arraysEqual(a1, a2) {
+  if (a1.length !== a2.length) {
+    return false;
   }
 
-  Storage.prototype.setItem.apply(this, arguments);
+  return a1.every(function(v, i) {
+    return Array.isArray(v) && Array.isArray(a2[i]) ?
+      arraysEqual(v, a2[i]) : v === a2[i]
+  });
 }
+
+// Parse and return the PTT shortcut from a serialized MediaEngineStore
+// structure.
+function parseShortcut(storageValue) {
+  try {
+    const value = JSON.parse(storageValue).default;
+    return value.mode == 'PUSH_TO_TALK' ? value.modeOptions.shortcut : [];
+  } catch (err) {
+    console.error('Couldn\'t parse PTT shortcut: ' + err);
+    return [];
+  }
+}
+
+// Override method to notify us about local storage changes.
+window.localStorage.__proto__ = Object.create(Storage.prototype);
+window.localStorage.__proto__.setItem = (function() {
+  // Only notify about changes to shortcut.
+  let prevShortcut = null;
+
+  return function(key, value) {
+    if (key === 'MediaEngineStore') {
+      const curShortcut = parseShortcut(value);
+      if (prevShortcut === null || !arraysEqual(curShortcut, prevShortcut)) {
+        prevShortcut = curShortcut;
+        document.dispatchEvent(new CustomEvent('SwpttShortcutChanged', {
+          'detail': curShortcut
+        }));
+      }
+    }
+
+    Storage.prototype.setItem.apply(this, arguments);
+  }
+})();
