814863bbdac2f44420ee252dbf4ce55b4a13b997
Michael Martis <michaeljmartis@gmail.com>
2020-07-31 01:17:31 +0800

    Added minimum mic on length

--- background.js
+++ background.js
@@ -3,23 +3,23 @@ let tabs = new Set();
 // Track which tabs have Discord loaded.
 // TODO: sort out callback usage.
 chrome.runtime.onMessage.addListener(function(message, sender, cb) {
-	cb();
+  cb();
 
-	if (!sender.tab || !sender.tab.id) {
-		return false;
-	}
+  if (!sender.tab || !sender.tab.id) {
+    return false;
+  }
 
-	tabs.add(sender.tab.id);
-	return false;
+  tabs.add(sender.tab.id);
+  return false;
 });
 
 // Automatically remove tab mapping on closed tabs.
 chrome.tabs.onRemoved.addListener(function(id, _) {
-	tabs.delete(id);
+  tabs.delete(id);
 });
 
 chrome.commands.onCommand.addListener(function(command) {
-	for (const id of tabs) {
-		chrome.tabs.sendMessage(id, '', () => {});
-	}
+  for (const id of tabs) {
+    chrome.tabs.sendMessage(id, '', () => {});
+  }
 });
--- content.js
+++ content.js
@@ -1,16 +1,18 @@
-// The number of ms to wait without shortcut messages before ending PTT.
-const PTT_OFF_DELAY = 800;
+// The minimum number of ms to wait from initial shortcut message before ending PTT.
+const PTT_DELAY_FIRST = 800;
+// The number of ms to wait from last shortcut message before ending PTT.
+const PTT_DELAY_LAST = 100;
 
 // A manually-constructed map from modifiers to their (likely) key codes. From
 // https://github.com/wesbos/keycodes/blob/gh-pages/scripts.js. 
 const MOD_KEY_CODES = [
-	["shiftKey", 16],
-	["ctrlKey", 17],
-	["altKey", 18],
-	["metaKey", 91],
+  ["shiftKey", 16],
+  ["ctrlKey", 17],
+  ["altKey", 18],
+  ["metaKey", 91],
 ];
 
-let pttDown = false;
+let pttEndTime = null;
 let keyInits = null;
 let toId = null;
 
@@ -18,70 +20,73 @@ let toId = null;
 const injected = document.createElement("script");
 injected.src = chrome.runtime.getURL('injected.js');
 injected.onload = function() {
-	this.remove();
+  this.remove();
 };
 (document.head || document.documentElement).appendChild(injected);
 
 // Listen for updates to page's PTT shortcut.
 document.addEventListener('BwpttShortcutChanged', function(ev) {
-	console.debug('PTT Shortcut Changed: ' + ev.detail);
+  console.debug('PTT Shortcut Changed: ' + ev.detail);
 
-	if (ev.detail === []) {
-		keyEventInits = null;
-		return;
-	}
+  if (ev.detail === []) {
+    keyEventInits = null;
+    return;
+  }
 
-	keyCodeList = ev.detail;
-	keyEventInits = {};
-	lastModKeyCode = -1;
-	for ([mod, modKeyCode] of MOD_KEY_CODES) {
-		const index = keyCodeList.indexOf(modKeyCode);
-		if (index === -1) {
-			continue;
-		}
+  keyCodeList = ev.detail;
+  keyEventInits = {};
+  lastModKeyCode = -1;
+  for ([mod, modKeyCode] of MOD_KEY_CODES) {
+    const index = keyCodeList.indexOf(modKeyCode);
+    if (index === -1) {
+      continue;
+    }
 
-		keyCodeList.splice(index, 1);
-		keyEventInits[mod] = true;
-		lastModKeyCode = modKeyCode;
-	}
+    keyCodeList.splice(index, 1);
+    keyEventInits[mod] = true;
+    lastModKeyCode = modKeyCode;
+  }
 
-	if (keyCodeList.length > 1) {
-		console.debug('Unknown mod key present: key code ' + keyCodeList);
-		return;
-	}
+  if (keyCodeList.length > 1) {
+    console.debug('Unknown mod key present: key code ' + keyCodeList);
+    return;
+  }
 
-	keyEventInits['keyCode'] = keyCodeList.length > 0 ? keyCodeList[0] : lastModKeyCode;
+  keyEventInits['keyCode'] = keyCodeList.length > 0 ? keyCodeList[0] : lastModKeyCode;
 });
 
 // Send a PTT keyup event. 
 function pttOff() {
-	pttDown = false;
+  pttEndTime = null;
 
-	if (keyEventInits !== null) {
-		document.dispatchEvent(new KeyboardEvent('keyup', keyEventInits));
-	}
+  if (keyEventInits !== null) {
+    document.dispatchEvent(new KeyboardEvent('keyup', keyEventInits));
+  }
 }
 
 // Extend the PTT off timeout, and send a PTT keydown event if one hasn't been
 // sent yet.
 // TODO sort out callback
 chrome.runtime.onMessage.addListener(function(_, _, cb) {
-	cb();
+  cb();
 
-	if (keyEventInits === null) {
-		return;
-	}
+  if (keyEventInits === null) {
+    return;
+  }
 
-	if (toId !== null) {
-		clearTimeout(toId);
-	}
-	toId = setTimeout(pttOff, PTT_OFF_DELAY);
+  if (toId !== null) {
+    clearTimeout(toId);
+  }
+  const pttDelay = pttEndTime === null ?
+    PTT_DELAY_FIRST :
+    Math.max(PTT_DELAY_LAST, pttEndTime  - new Date().getTime());
+  toId = setTimeout(pttOff, pttDelay);
 
-	if (!pttDown) {
-		pttDown = true;
-		document.dispatchEvent(new KeyboardEvent('keydown', keyEventInits));
-	}
+  if (pttEndTime === null) {
+    pttEndTime = new Date().getTime() + PTT_DELAY_FIRST;
+    document.dispatchEvent(new KeyboardEvent('keydown', keyEventInits));
+  }
 });
 
 // Notify background script that we're a Discord tab.
-chrome.runtime.sendMessage({}, ()=>{});
+chrome.runtime.sendMessage({}, () => {});
