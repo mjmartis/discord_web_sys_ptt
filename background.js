224283565d1e0e1eda65f6432c511d873f6f835e
Michael Martis <michaeljmartis@gmail.com>
2020-07-31 16:25:49 +0800

    Currently broken attempt at delay UI. Broadcasting window changed message to Discord tabs doesn't work.

--- background.js
+++ background.js
@@ -1,25 +1,72 @@
+let DEFAULT_WINDOW_VALUE = 800;
+
+// TODO remove and make extension non-persistent.
 let tabs = new Set();
 
-// Track which tabs have Discord loaded.
+// Propogates the stored window value, or the default (if no value has been stored).
+function sendWindowValue(cb) {
+	chrome.storage.local.get(['windowValue'], function(result) {
+		cb('windowValue' in result ? result.windowValue : DEFAULT_WINDOW_VALUE);
+	});
+}
+
+// Called when a Discord tab is loaded.
+function onDiscordLoaded(sender, cb) {
+	if (!sender.tab || !sender.tab.id) {
+		cb();
+		return false;
+	}
+	tabs.add(sender.tab.id);
+
+	sendWindowValue(cb);
+	return true;
+}
+
+// Called when a popup is loaded.
+function onPopupLoaded(cb) {
+	sendWindowValue(cb);
+	return true;
+}
+
+// Called when the window value is changed.
+function onWindowChanged(windowValue, cb) {
+	chrome.storage.local.set({
+		windowValue: windowValue
+	});
+	chrome.runtime.sendMessage({
+		id: 'window_changed',
+		value: windowValue
+	});
+
+	cb();
+	return false;
+}
+
 // TODO: sort out callback usage.
-chrome.runtime.onMessage.addListener(function(message, sender, cb) {
-  cb();
-
-  if (!sender.tab || !sender.tab.id) {
-    return false;
-  }
-
-  tabs.add(sender.tab.id);
-  return false;
+chrome.runtime.onMessage.addListener(function(msg, sender, cb) {
+	if (msg.id === 'discord_loaded') {
+		return onDiscordLoaded(sender, cb);
+	} else if (msg.id === 'popup_loaded') {
+		return onPopupLoaded(cb);
+	} else if (msg.id === 'window_changed') {
+		console.log('background got window changed');
+		return onWindowChanged(msg.value, cb);
+	}
+	
+	cb();
+	return false;
 });
 
 // Automatically remove tab mapping on closed tabs.
 chrome.tabs.onRemoved.addListener(function(id, _) {
-  tabs.delete(id);
+	tabs.delete(id);
 });
 
-chrome.commands.onCommand.addListener(function(command) {
-  for (const id of tabs) {
-    chrome.tabs.sendMessage(id, '', () => {});
-  }
+// When extension shortcut is pressed, notify Discord tabs.
+chrome.commands.onCommand.addListener(function(_) {
+	for (const id of tabs) {
+		chrome.tabs.sendMessage(id, {
+			id: 'ext_shortcut'
+		}, () => {});
+	}
 });
---
+++ popup.js
@@ -0,0 +1,35 @@
+var slider = document.getElementById('slider');
+var label = document.getElementById('label');
+
+// Called when a new window value is set from any popup.
+function onWindowValueChanged(windowValue) {
+  slider.value = windowValue;
+  label.innerHTML = windowValue + 'ms';
+}
+
+// Keep our window value in sync with all the others.
+chrome.runtime.onMessage.addListener(function(msg, _, cb) {
+  cb();
+
+  if (msg.id !== 'window_changed') {
+    console.debug('Unexpected message "' + msg.id + '" received by popup.');
+    return false;
+  }
+
+  onWindowValueChanged(msg.value);
+  return false;
+});
+
+// Request stored window value from background script.
+chrome.runtime.sendMessage({
+  id: 'popup_loaded'
+}, onWindowValueChanged);
+
+// Update background (and subsequently all other popups) with new values from
+// this slider.
+slider.oninput = function() {
+  chrome.runtime.sendMessage({
+    id: 'window_changed',
+    value: this.value
+  }, () => {});
+}
