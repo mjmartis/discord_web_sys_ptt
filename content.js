f9a78880924ff2838eb11a40f8dcf8a33bd1da7f
Michael Martis <michaeljmartis@gmail.com>
2020-08-05 19:02:23 +0800

    Fixed race condition

--- content.js
+++ content.js
@@ -23,6 +23,82 @@ const MOD_KEY_CODES = [
   ['metaKey', 91],
 ];
 
+const INJECTED_JS = String.raw`
+/**
+ * Value used to specify keyboard shortcuts in the Discord web client.
+ * @const {number}
+ */
+const DISCORD_KEYBOARD = 0;
+
+/**
+ * Value used to specify browser shortcuts in the Discord web client.
+ * @const {number}
+ */
+const DISCORD_BROWSER = 4;
+
+/**
+ * Parses and returns the PTT shortcut from a serialized MediaEngineStore
+ * structure.
+ *
+ * @param {?Object} storageValue - The string value associated with the
+ *     MediaEngineStore key in local storage, or null if no such key exists in
+ *     local storage.
+ * @return {!Array<number>} The list of key codes specified as the PTT shortcut
+ *     in the MediaEngineStore structure, or an empty list if PTT is not
+ *     enabled or there was an error.
+ */
+function parseShortcut(storageValue) {
+  // There will be no MediaEngineStore entry on first usage of the Discord web
+  // client.
+  if (storageValue == null) return [];
+
+  try {
+    const value = JSON.parse(storageValue).default;
+    if (value.mode !== 'PUSH_TO_TALK') {
+      return [];
+    }
+
+    // Return a list of key codes, from the list with entries of the form:
+    //   [KEYBOARD, key code, BROWSER].
+    return value.modeOptions.shortcut.map(function(vs) {
+      if (vs.length != 3 || vs[0] != DISCORD_KEYBOARD || vs[2] != DISCORD_BROWSER) {
+        throw new Error("unrecognised shortcut specification.");
+      }
+      return vs[1];
+    }).sort();
+  } catch (err) {
+    console.error('Couldn\'t parse PTT shortcut: ' + err.message);
+    return [];
+  }
+}
+
+// Overrides method to notify extension about local storage changes.
+window.localStorage.__proto__ = Object.create(Storage.prototype);
+window.localStorage.__proto__.setItem = (function() {
+  // Notify about initial PTT shortcut.
+  const initShortcut = parseShortcut(window.localStorage.getItem('MediaEngineStore'));
+  document.dispatchEvent(new CustomEvent('BwpttShortcutChanged', {
+    'detail': initShortcut,
+  }));
+
+  // Then only notify about changes to shortcut.
+  let prevShortcut = initShortcut;
+  return function(key, value) {
+    if (key === 'MediaEngineStore') {
+      const curShortcut = parseShortcut(value);
+      if (curShortcut !== prevShortcut) {
+        prevShortcut = curShortcut;
+        document.dispatchEvent(new CustomEvent('BwpttShortcutChanged', {
+          'detail': curShortcut,
+        }));
+      }
+    }
+
+    Storage.prototype.setItem.apply(this, arguments);
+  };
+})();
+`;
+
 /**
  * The minimum number of ms to wait from initial shortcut message before ending
  * PTT.
@@ -49,15 +125,6 @@ let keyEventInits = null;
  * @type {?number}
  */
 let toId = null;
-
-// Inject script to run in page's JS environment.
-const injected = document.createElement('script');
-injected.src = chrome.runtime.getURL('injected.js');
-/** Removes the injected script once it has executed. */
-injected.onload = function() {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(injected);
 
 // Listen for updates to page's PTT shortcut.
 document.addEventListener('BwpttShortcutChanged', function(ev) {
@@ -134,3 +201,9 @@ chrome.runtime.sendMessage({
 }, minPttLength => {
   pttDelayFirst = minPttLength;
 });
+
+// Inject script to run in page's JS environment.
+const injected = document.createElement('script');
+injected.textContent = INJECTED_JS;
+(document.head || document.documentElement).appendChild(injected);
+injected.remove();
